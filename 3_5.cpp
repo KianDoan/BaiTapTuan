357b54e35353469bc164e17fada4737ff10c4b7f
Trung Kien <22028211@vnu.edu.vn>
2023-02-22 14:56:32 +0700

    tuan 3

--- 3_5.cpp
+++ 3_5.cpp
@@ -1,30 +1,42 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     int m,n;
     cin>>m>>n;
-    int** arr=new int*[m];
-    for(int i=0;i<m;i++)
-        arr[i]=new int[n];
-    int val=0;
-    while(val<=m*n){
-        int i=0, j=0,minRow=0,maxRow=m-1,minCol=0,maxCol=n-1;
-        val++;
-        arr[i][j]=val;
-        if(i==minRow && j!=maxCol)
-            j++;
-        else if(j==maxCol && i!=maxRow)
-            i++;
-        else if(i==maxRow && j!=minCol)
-            j--;
-        else if(j==minCol && i!=minRow) 
-            i--;
+    int arr[10][10];
+    int val=1;
+    int minRow=0,minCol=0,maxRow=m-1,maxCol=n-1;
+    while(val <= (m*n)){
+        for(int i=minCol;i<=maxCol;i++){
+            arr[minRow][i]=val;
+            val++;
+        }
+        minRow++;
+        for(int i=minRow;i<=maxRow;i++){
+            arr[i][maxCol]=val;
+            val++;
+        }
+        maxCol--;
+        for(int i=maxCol;i>=minCol;i--){
+            arr[maxRow][i]=val;
+            val++;
+        }
+        maxRow--;
+        for(int i=maxRow;i>=minRow;i--){
+            arr[i][minCol]=val;
+            val++;
+        }
+        minCol++;
     }
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++)
             cout<<arr[i][j]<<" ";
         cout<<endl;
     }
+    for(int i=0;i<m;i++)
+        delete[] arr[i];
+    delete[] arr;
+    return 0;
 }
